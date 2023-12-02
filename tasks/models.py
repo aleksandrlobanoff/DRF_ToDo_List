129bc432d47d8465833f07b00239499ad3dcc735
alexandr.lobanov <alexandp83@mail.ru>
2023-12-02 12:57:56 +0300

    added task model

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+
+        return f"{self.title}, {self.description}, {self.created_at}"
+
+    class Meta:
+        verbose_name = "task"
+        verbose_name_plural = "tasks"
