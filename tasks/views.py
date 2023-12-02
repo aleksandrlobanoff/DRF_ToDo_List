1f248437f0b18ad918770d96a438cf6b3527c9c5
alexandr.lobanov <alexandp83@mail.ru>
2023-12-02 13:58:08 +0300

    added sorting and filtering of tasks by various attributes

--- tasks/views.py
+++ tasks/views.py
@@ -1,10 +1,12 @@
 from rest_framework import viewsets
+from rest_framework import filters
 
 from tasks.models import Task
 from tasks.serializers import TaskSerializer
 
 
 class TaskViewSet(viewsets.ModelViewSet):
-    queryset = Task.objects.all()
+    queryset = Task.objects.all().order_by('created_at')
     serializer_class = TaskSerializer
-
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title', 'description', 'created_at']
